1a30e3171ca451d43853c87007aceb5e5c2d3b9e
BAE <lexon1209@gmail.com>
2025-01-14 16:36:22 +0900

    Initial Commit

---
+++ AlamofirePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofirePractice
+//
+//  Created by BAE on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
